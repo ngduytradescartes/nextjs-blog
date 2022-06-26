0abb3ad350618d8c4ffe6076c9842fa10d131fad
ngduytradescartes <95331134+ngduytradescartes@users.noreply.github.com>
2022-06-26 10:48:05 +0700

    .

---
+++ pages/404.js
@@ -0,0 +1,3 @@
+export default function Custom404() {
+  return <h1>Hello, this page is not found</h1>;
+}
---
+++ pages/posts/[id].js
@@ -0,0 +1,44 @@
+import Layout from "../../components/layout";
+import { getAllPostIds, getPostData } from "../../lib/posts";
+import Head from "next/head";
+import Date from "../../components/date";
+import utilStyles from "../../styles/utils.module.css";
+
+export default function Post({ postData }) {
+  return (
+    <Layout>
+      <Head>
+        <title>{postData.title}</title>
+        <meta property="og:title" content={postData.title} />
+        <meta property="og:description" content={postData.title} />
+        <meta property="og:image" content={postData.thumnail} />
+        <meta property="og:image:width" content="828" />
+        <meta property="og:image:height" content="434" />
+      </Head>
+      <article>
+        <h1 className={utilStyles.headingXl}>{postData.title}</h1>
+        <div className={utilStyles.lightText}>
+          <Date dateString={postData.date} />
+        </div>
+        <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+      </article>
+    </Layout>
+  );
+}
+
+export async function getStaticPaths() {
+  const paths = getAllPostIds();
+  return {
+    paths,
+    fallback: false,
+  };
+}
+
+export async function getStaticProps({ params }) {
+  const postData = await getPostData(params.id);
+  return {
+    props: {
+      postData,
+    },
+  };
+}
